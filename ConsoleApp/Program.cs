002724336af96a3c7c1cc7c00b5470cdbb20ae8c
Leszek02 <leszek.miernicki02@gmail.com>
2024-10-23 21:45:55 +0200

    add lab3 solution

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace ConsoleApp;
-
+﻿using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
+using System.Configuration;
+namespace ConsoleApp;
+//Install configuration manager with NuGet
 class Program
 {
     static void Main(string[] args)
     {
-        Interfaces.IDataAccessObject dao = new DAOMock2.DAOMock2();
+        string libname = System.Configuration.ConfigurationManager.AppSettings["library_name"];
+        //!!!!WAZNE!!!!! change to args[0] on Windows
+        //args[1];
+        Assembly assembly = Assembly.UnsafeLoadFrom(libname);
+        Type typeToCreate = null;
+        //Console.WriteLine(Configuration) //Here somehow access dao.config file
+        foreach ( var t in assembly.GetTypes())
+        {
+            if ( t.IsAssignableTo( typeof(Interfaces.IDataAccessObject)))
+            {
+                typeToCreate = t;
+                break;
+            }
+        }
+
+        Interfaces.IDataAccessObject dao = (Interfaces.IDataAccessObject) Activator.CreateInstance(typeToCreate);
 
         #region car service
         foreach (Interfaces.IProducer p in dao.GetAllProducers())
